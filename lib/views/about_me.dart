ea3ce2e8290c1aea75e72f1395bf6af329305645
archp4 <archpatel4@gmail.com>
2022-04-25 00:02:44 +0530

    about me mobile responive

--- lib/views/about_me.dart
+++ lib/views/about_me.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:portfilo/models/responstive_helper.dart';
 import 'package:portfilo/widget/custom_textstyle.dart';
 import 'package:portfilo/widget/header_text.dart';
 import 'package:portfilo/widget/highlight_section.dart';
@@ -7,29 +8,7 @@ import 'package:portfilo/widget/hire_buttom.dart';
 class AboutMe extends StatelessWidget {
   const AboutMe({Key? key}) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    double height = MediaQuery.of(context).size.height;
-    return Container(
-      alignment: Alignment.center,
-      child: SizedBox(
-        width: height < 800 ? 1020 : 1120,
-        child: Row(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            _image(height),
-            Container(
-              margin: const EdgeInsets.only(left: 20),
-              width: height < 800 ? 500 : 600,
-              child: _data(),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-
-  Widget _data() {
+  Widget _data(context) {
     return Column(
       mainAxisSize: MainAxisSize.min,
       mainAxisAlignment: MainAxisAlignment.start,
@@ -46,6 +25,9 @@ class AboutMe extends StatelessWidget {
         Text(
           "Hi! I’m Arch Patel, and I’m Flutter developer who has passion for building clean web applications with intuitive functionality. I enjoy the process of turning ideas into reality using creative solutions. I’m always curious about learning new skills, tools, and concepts. In addition to working on various solo full stack projects, I have worked with creative teams, which involves daily stand-ups and communications, source control, and project management.",
           style: bodyTextStyle(),
+          textAlign: ResponstiveHelper.isMobile(context)
+              ? TextAlign.justify
+              : TextAlign.left,
         ),
         const SizedBox(
           height: 100,
@@ -60,6 +42,48 @@ class AboutMe extends StatelessWidget {
       "assets/images/yellow_me_2.png",
       width: height < 800 ? 500 : 500,
       height: height < 800 ? 500 : 500,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    double height = MediaQuery.of(context).size.height;
+
+    if (!ResponstiveHelper.isMobile(context)) {
+      return Container(
+        alignment: Alignment.center,
+        child: SizedBox(
+          width: height < 800 ? 1020 : 1120,
+          child: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              _image(height),
+              Container(
+                margin: const EdgeInsets.only(left: 20),
+                width: height < 800 ? 500 : 600,
+                child: _data(context),
+              ),
+            ],
+          ),
+        ),
+      );
+    }
+    return Container(
+      alignment: Alignment.center,
+      child: SizedBox(
+        width: height < 800 ? 1020 : 1120,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            _image(height),
+            Container(
+              margin: const EdgeInsets.only(left: 20, right: 20),
+              width: height < 800 ? 500 : 600,
+              child: _data(context),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
